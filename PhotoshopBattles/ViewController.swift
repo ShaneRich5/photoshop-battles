831a31403ac6825b9e936a417589a983f8aa8259
Shane Richards <shane.richards@fticonsulting.com>
2020-07-30 00:43:38 -0400

    Initial Commit

---
+++ PhotoshopBattles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoshopBattles
+//
+//  Created by Shane Richards on 7/30/20.
+//  Copyright © 2020 Shane Richards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
