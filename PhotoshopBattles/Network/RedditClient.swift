37217ec952a128fc5fb7c1cadf1b85daa50a41ab
Shane Richards <shane.richards@fticonsulting.com>
2020-08-01 05:25:06 -0400

    feat: created constants for the reddit client

---
+++ PhotoshopBattles/Network/RedditClient.swift
@@ -0,0 +1,18 @@
+//
+//  RedditClient.swift
+//  PhotoshopBattles
+//
+//  Created by Shane Richards on 8/1/20.
+//  Copyright © 2020 Shane Richards. All rights reserved.
+//
+
+import Foundation
+
+class RedditClient {
+    struct Constants {
+        static let BASE_URL = "https://www.reddit.com/api/v1/access_token"
+        static let CLIENT_ID = "ArB2sddOUAh0CA"
+        static let CLIENT_SECRET = ""
+        static let GRANT_TYPE = "https://oauth.reddit.com/grants/installed_client"
+    }
+}
--- PhotoshopBattles/ViewController.swift
+++ PhotoshopBattles/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        debugPrint(UIDevice.current.identifierForVendor?.uuidString)
+        
+//        let urlString = "https://www.reddit.com/r/photoshopbattles.json"
+//        AF.request(urlString).response { response in
+//            let json = try? JSON(data: response.data!)
+//            debugPrint(json)
+//        }
     }
 
 
