e9d1fd5df80eade60c8243f7fc663e10f583665a
Shane Richards <shane.richards121@gmail.com>
2025-07-13 19:29:55 -0400

    chore: updating the wording

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import Image from "next/image";
 import ContestList from "@/components/contest-list";
 
 export default function Home() {
   return (
     <div className="pt-4">
       <main className="max-w-6xl mx-auto px-4 py-8">
-        <h1>Contest List</h1>
-        {/* Create a grid for the images in*/}
-        <h1 className="text-2xl font-bold mb-4">Photoshop Battles</h1>
+        <h1 className="text-2xl font-bold mb-0">r/PhotoshopBattles</h1>
         <p className="text-gray-600 mb-8">
-          A collection of the latest Photoshop Battles from Reddit.
+          A simple feed for people too lazy to open the individual links within
+          the subreddit.
         </p>
         <ContestList />
       </main>
