06144538091c3cc8a4065a20088c2ff420cc8adf
Shane Richards <shane.richards121@gmail.com>
2025-07-14 19:35:53 -0400

    feat: adding user agents

--- lib/api-clients/reddit.ts
+++ lib/api-clients/reddit.ts
@@ -29,7 +29,7 @@ const fetchWithHeaders = async (
 ): Promise<Response> => {
   const defaultHeaders: HeadersInit = {
     "Content-Type": "application/json",
-    Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
+    "User-Agent": "PhotoshopBattles Feed/0.1",
   };
 
   const mergedOptions: RequestInit = {
