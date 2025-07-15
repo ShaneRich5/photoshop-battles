e8cac00326b992f6ce739270670d74be03ccca16
Shane Richards <shane.richards121@gmail.com>
2025-07-14 23:50:08 -0400

    refactor: removing unused code

--- app/contests/[contestId]/page.tsx
+++ app/contests/[contestId]/page.tsx
@@ -1,6 +1,4 @@
-import { fetchRedditPostById } from "@/lib/api-clients/reddit";
-import Link from "next/link";
-import { MdArrowBack } from "react-icons/md";
+import ContestDetail from "@/components/contest-detail";
 
 interface ContestDetailPageProps {
   params: Promise<{
@@ -12,46 +10,10 @@ export default async function ContestDetailPage({
   params,
 }: ContestDetailPageProps) {
   const { contestId } = await params;
-  const { contest, submissions } = await fetchRedditPostById(contestId);
 
   return (
     <div className="pt-4">
-      <main className="max-w-6xl mx-auto px-4 py-8">
-        <div className="absolute top-0 left-0 p-4">
-          <Link href="/" className="flex items-center mb-6">
-            <MdArrowBack className="text-xl cursor-pointer" />
-            <span className="text-gray-600 ml-2 text-lg font-bold">
-              Back to Contests
-            </span>
-          </Link>
-        </div>
-        <div className="flex flex-col items-center">
-          <img
-            src={contest.imageUrl}
-            alt={contest.title}
-            className="h-96 mb-4 object-cover rounded-lg"
-          />
-          <h2 className="text-xl font-bold mb-0">{contest.title}</h2>
-          <p className="text-gray-600 mb-8">by {contest.author || "Unknown"}</p>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {submissions
-            .filter((submission) => submission.imageUrl)
-            .map((submission: any) => (
-              <div key={submission.id} className="text-center ">
-                <img
-                  src={submission.imageUrl}
-                  alt={submission.title}
-                  className="w-full object-cover rounded-lg"
-                />
-                <h2 className="text-md font-semibold mt-2">
-                  {submission.title}
-                </h2>
-                <p className="text-gray-600 text-sm">by {submission.author}</p>
-              </div>
-            ))}
-        </div>
-      </main>
+      <ContestDetail contestId={contestId} />
     </div>
   );
 }
