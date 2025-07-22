b0aa3cb5ef7f0e87324db081f0f595b0b8d307dd
Shane Richards <shane.richards121@gmail.com>
2025-07-22 02:24:03 -0400

    feat: temporarily removing the header component to resolve a production error

--- app/contests/[contestId]/page.tsx
+++ app/contests/[contestId]/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { MdArrowBack } from "react-icons/md";
 import { fetchRedditSubmissionPostById } from "../../../lib/api-clients/reddit";
-import ContestHeaderSummary from "../../../components/contest-header-summary";
+// import ContestHeaderSummary from "../../../components/contest-header-summary";
 
 interface ContestDetailPageProps {
   params: Promise<{
@@ -26,9 +26,9 @@ export default async function ContestDetailPage({
             </span>
           </Link>
         </div>
-        <div className="mb-8">
+        {/* <div className="mb-8">
           <ContestHeaderSummary contestId={contestId} />
-        </div>
+        </div> */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {submissions
             .filter((submission) => submission.imageUrl)
